c8a1cfb68d5dfb4e3d3e781f0d32b67bfa3a45e9
codingxperience <oko22002@byui.edu>
2023-04-01 23:59:37 +0300

    render deploy

--- swagger.js
+++ swagger.js
@@ -5,12 +5,19 @@ const doc = {
     title: 'My API',
     description: 'Book-store API',
   },
-  host: 'localhost:3000',
-  schemes: ['http'],
+  host: 'https://offs341-05.onrender.com',
+  schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
+
+// localhost:3000
+// schemes: ['http']
+
+// host: 'https://offs341-05.onrender.com',
+// schemes: ['https']
+
 
 /* NOTE: if you use the express Router, you must pass in the 
    'endpointsFiles' only the root file where the route starts,
