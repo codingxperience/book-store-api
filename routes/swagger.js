9f427a9045ad68e0e821c8a8bcaac164e7901341
codingxperience <oko22002@byui.edu>
2023-04-02 01:04:53 +0300

    refactored render swagger

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
-const { ensureAuth } = require("../middleware/auth");
+// const { ensureAuth } = require("../middleware/auth");
 
 const swaggerDocument = require('../swagger.json');
 
-router.use('/api-docs', ensureAuth, swaggerUi.serve);
+router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
